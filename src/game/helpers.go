29ba0a41ea5389e44ea0b307b07556e4d47d6f91
zero13cool <zero13cool@yandex.ru>
2018-08-27 20:43:52 +0100

    Don't store anything by default

--- src/game/helpers.go
+++ src/game/helpers.go
@@ -4,14 +4,13 @@ import "game/models"
 
 // Creates an empty scene
 func CreateScene(width int, height int) *models.Scene {
-	brick := models.Brick{}
 	matrix := [][]models.GameObject{}
 
 	for i := 0; i < height; i++ {
 		tmp := make([]models.GameObject, width)
 
 		for x, _ := range tmp {
-			tmp[x] = &brick
+			tmp[x] = nil
 		}
 
 		matrix = append(matrix, tmp)
