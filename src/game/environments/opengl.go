d015eb518645cee52daa2380b345998b4f890060
zero13cool <zero13cool@yandex.ru>
2019-02-17 20:28:41 +0000

    Drawing some triangles

--- src/game/environments/opengl.go
+++ src/game/environments/opengl.go
@@ -1,8 +1,158 @@
 package environments
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"runtime"
+	"strings"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+var (
+	triangleOne = []float32{
+		-1, 0, 0,
+		0, -1, 0,
+		1, 0, 0,
+	}
+
+	triangleTwo = []float32{
+		-1, 0, 0,
+		0, 0.5, 0,
+		0, 0, 0,
+	}
+
+	vertexShaderSource = `
+		#version 410
+		in vec3 vp;
+		void main() {
+			gl_Position = vec4(vp, 1.0);
+		}
+	` + "\x00"
+
+	fragmentShaderSource = `
+		#version 410
+		out vec4 frag_colour;
+		void main() {
+			frag_colour = vec4(0.5, 29, 120, 171);
+		}
+	` + "\x00"
+)
 
 // InitOpenGL inits the game as OpenGL application.
 func InitOpenGL(isServer bool, host string, avatar string, name string, isBot bool) {
-	fmt.Println("OpenGL!")
+	runtime.LockOSThread()
+
+	window := initGlfw()
+	defer glfw.Terminate()
+
+	program := _initOpenGL()
+
+	vaoOne := makeVao(triangleOne)
+	vaoTwo := makeVao(triangleTwo)
+
+	for !window.ShouldClose() {
+		draw(window, program, []uint32{vaoOne, vaoTwo})
+	}
+}
+
+func _initOpenGL() uint32 {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Printf("OpenGL version %v", version)
+
+	vertexShader, err := compileSharer(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	fragmentShader, err := compileSharer(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	p := gl.CreateProgram()
+	gl.AttachShader(p, vertexShader)
+	gl.AttachShader(p, fragmentShader)
+	gl.LinkProgram(p)
+
+	return p
+}
+
+func initGlfw() *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(640, 480, "Game", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	return window
+}
+
+func draw(window *glfw.Window, program uint32, vaos []uint32) {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(program)
+
+	for _, vao := range vaos {
+		gl.BindVertexArray(vao)
+		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangleOne)/3))
+	}
+
+	glfw.PollEvents()
+	window.SwapBuffers()
+}
+
+// makeVao makes vertex array from the points provided.
+func makeVao(points []float32) uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+
+	return vao
+}
+
+func compileSharer(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLen int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLen)
+
+		log := strings.Repeat("\x00", int(logLen+1))
+		gl.GetShaderInfoLog(shader, logLen, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
 }
