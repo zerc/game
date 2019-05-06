9d57e262d52dc26095ae7a8f6a0b04d5abf211ec
zero13cool <zero13cool@yandex.com>
2019-05-06 18:47:47 +0100

    The first sphere is here

--- src/omega/main.cpp
+++ src/omega/main.cpp
@@ -1,19 +1,56 @@
+#include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include "o_math.hpp"
 
-const int width    = 1024;
-const int height   = 768;
+const float width    = 200;
+const float height   = 200;
+const float fov    = 1;  // tan(90/2) i.e. FOV is 90 degree
+auto aspect_ratio = width / height;
+
+/*
+Logic:
+
+1. Cast rays: from origin (0, 0, 0) to center of each pixel from the framebuffer.
+2. For each ray need to check either it intersects with the Sphere or not
+3. Render: based on [2] draw pixels differently
+
+Objects:
+
+    Vector - just to work with (x, y, z)
+    Sphere - to represent an object (center, radius)
+*/
 
 void render(sf::RenderWindow& window)
 {
     sf::VertexArray framebuffer(sf::Points, width*height);
 
+    omega::Vector origin(0, 0, 0);  // of the camera
+
+    // Sphere
+    omega::Vector center(0, 0, -10); // WTF is this coordinates?
+    omega::Sphere sphere(center, 2);
+
+    // Cast rays
     for (size_t j = 0; j<height; j++) {
         for (size_t i = 0; i<width; i++) {
             int index = i+j*width;
             framebuffer[index].position = sf::Vector2f(i, j);
-            framebuffer[index].color = sf::Color::Blue;
+
+            // Convert a pixel's coordinates to screen space with account for aspect ratio and FOV
+            // https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-generating-camera-rays/generating-camera-rays
+            float x = (2 * (i + 0.5) / width - 1) * aspect_ratio * fov;
+            float y = (1 - 2 * (j + 0.5) / height) * fov;
+
+            omega::Vector dest(x, y, -1);
+            dest.Normalize();
+
+            if (sphere.intersects(origin, dest)) {
+                framebuffer[index].color = sf::Color::Red;
+            } else {
+                framebuffer[index].color = sf::Color::Blue;
+            }
         }
     }
 
@@ -36,7 +73,7 @@ int main()
                 window.close();
         }
 
-        window.clear(sf::Color::Red);
+        window.clear(sf::Color::Green);
         render(window);
         window.display();
     }
---
+++ src/omega/o_math.hpp
@@ -0,0 +1,75 @@
+#include <math.h>
+
+namespace omega {
+    class Vector {
+        float x, y, z;
+        bool normalized;
+
+        public:
+            Vector (float x_, float y_, float z_) : x(x_), y(y_), z(z_) {
+                normalized = false;
+            };
+
+            Vector operator -(const Vector& v) {
+                return Vector(x - v.x, y - v.y, z - v.z);
+            };
+
+            Vector operator +(const Vector& v) {
+                return Vector(x + v.x, y + v.y, z + v.z);
+            };
+
+            // Calculates dot product between current vector and the given one (v)
+            float Dot (const Vector& v) {
+                return x*v.x + y*v.y + z*v.z;
+            };
+
+            float Magnitude() {
+                return std::sqrt(x*x + y*y + z*z);
+            };
+
+            void Normalize() {
+                float m = Magnitude();
+                x = x/m;
+                y = y/m;
+                z = z/m;
+                normalized = true;
+            };
+
+            bool IsNormalised() {
+                return normalized;
+            };
+    };
+
+    class Sphere {
+        Vector& center;
+        float radius;
+
+        public:
+            Sphere (Vector& c, float r) : center(c), radius(r) {};
+            bool intersects(Vector& origin, Vector& dest);
+    };
+
+    /*
+     * Returns either the vector given by (origin, dest) is intersecting with the shpere or not.
+     * http://www.lighthouse3d.com/tutorials/maths/ray-sphere-intersection/ 
+     * https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection
+     * NOTE: `dest` is expected to be normalised vector
+     */
+    bool Sphere::intersects (Vector& origin, Vector& dest) {
+        auto L = (center - origin);
+        float tca = L.Dot(dest);
+
+        if (tca < 0) {
+            return false;
+        }
+
+        float d = L.Dot(L) - tca*tca;
+        float r = radius * radius;
+
+        if (d > r) {
+            return false;
+        }
+
+        return true;
+    }
+};
