d26eb1deb79a2db951ad4dd2086869fb02b038c2
zero13cool <zero13cool@yandex.ru>
2019-06-29 20:15:57 +0100

    Added SFML via Conan

--- src/omega/main.cpp
+++ src/omega/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
-#include "yaml-cpp/yaml.h"
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <yaml-cpp/yaml.h>
 
 int main() {
+    sf::RenderWindow window(sf::VideoMode(100, 100), "Test");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.resetGLStates();
+        window.clear(sf::Color::Green);
+        window.display();
+    }
+
     YAML::Node config = YAML::LoadFile("config.yaml");
     std::cout << config << std::endl;
     return 0;
